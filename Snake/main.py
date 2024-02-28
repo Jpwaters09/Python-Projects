ee71f63a248feaa0e9d862d9b9bd30a946d5f043
Jacob Waters <jpwaters.github@gmail.com>
2024-02-28 19:51:09 +0000

    Update main.py

--- Snake/main.py
+++ Snake/main.py
@@ -1,15 +1,15 @@
-##############################################
-# Snake                                      #
-# Author: Jacob Waters                       #
-# Github: github.com/jpwaters09              #
-# Contact me: jpwaters.github@gmail.com      #
-# Follow my WhatsApp Channel: bit.ly/3sr99ZO #
-##############################################
+################################################
+## Snake                                      ##
+## Author: Jacob Waters                       ##
+## Github: github.com/jpwaters09              ##
+## Contact me: jpwaters.github@gmail.com      ##
+## Follow my WhatsApp Channel: bit.ly/3sr99ZO ##
+################################################
 
-###################### For this code to work you have to install the pillow package see below on how to install it: #######################
-###### 1. Open the terminal by typing "cmd" for windows or "terminal" for linux depending on your operating system in the search bar. #####
-######################################### 2. Type "pip install pillow" followed by enter. #################################################
-###########################################################################################################################################
+###################### For this code to work you have to install the pillow package, see below on how to install it: #######################
+###### 1. Open the terminal by typing "cmd" for windows or "terminal" for linux depending on your operating system in the search bar. ######
+######################################### 2. Type "pip install pillow" followed by enter. ##################################################
+############################################################################################################################################
 
 import tkinter as tk
 from random import randint
